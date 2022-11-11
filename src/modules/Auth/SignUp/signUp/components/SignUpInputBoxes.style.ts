29146f8f78e039d651ff692e8f1173dc9af635f4
대왕더덕 <duduki@daewangdeodeog-ui-MacBookAir.local>
2022-11-11 23:33:50 +0900

    remove auth for rename

--- src/modules/Auth/SignUp/signUp/components/SignUpInputBoxes.style.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import { ThemeProps } from "../../../../../utils/theme/theme.interface";
-
-export const SignUpInputBoxesWrapper = styled.div<ThemeProps>(() => ({
-  flex: 1,
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "space-between",
-}));
